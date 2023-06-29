57c9f90000f497b99fd3209277e08cc55d9d32ad
Mchnan <chengsiyuan@acoinfo.com>
2023-06-30 01:16:23 +0800

    Add async & sync rpc call mixed test

--- client/client_async_rpc_test.go
+++ client/client_async_rpc_test.go
@@ -61,6 +61,77 @@ func TestRPCAsync(t *testing.T) {
 	}
 }
 
+func TestRPCMixed(t *testing.T) {
+	flag.Parse()
+
+	clientOption := Option{
+		Password: "123456",
+	}
+
+	c := NewClient(clientOption)
+	_, err := c.Connect("tcp", *rpc_async_addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	reqSync := protocol.NewMessage()
+	req1 := protocol.NewMessage()
+	req2 := protocol.NewMessage()
+	reply := protocol.NewMessage()
+
+	reqSync.URL = []byte("/a/b/c")
+	req1.URL = []byte("/a/b/c")
+	req2.URL = []byte("/a/b/c")
+
+	biddata := &RpcAsyncTestParam{
+		BigData: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+	}
+	req1.Param, _ = json.Marshal(biddata)
+	biddata = &RpcAsyncTestParam{
+		BigData: "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+	}
+	req2.Param, _ = json.Marshal(biddata)
+
+	reply, err = c.Call(protocol.TypeRPC, protocol.RpcMethodGet, reqSync)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		if reply.SeqNo() != 1 {
+			t.Fatal("Not sync")
+		}
+	}
+
+	// Actually We don't need to care Call1 using seq:1 or not, since it's async call
+	Call1 := c.Go(protocol.TypeRPC, protocol.RpcMethodGet, req1, reply, nil).Done
+	Call2 := c.Go(protocol.TypeRPC, protocol.RpcMethodGet, req2, reply, nil).Done
+
+	reply, err = c.Call(protocol.TypeRPC, protocol.RpcMethodGet, reqSync)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		if reply.SeqNo() != 2 {
+			t.Fatal("Not sync")
+		}
+	}
+
+	for i := 0; i < 2; i++ {
+		select {
+		case <-Call1:
+		case <-Call2:
+		}
+	}
+
+	reply, err = c.Call(protocol.TypeRPC, protocol.RpcMethodGet, reqSync)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		if reply.SeqNo() != 5 {
+			t.Fatal("Not sync")
+		}
+	}
+}
+
 func logAsyncCall(call *RpcCall, t *testing.T) {
 	reply := call.Reply
 	SrcParam := new(RpcAsyncTestParam)
