7fa6c3388158810926a481b02d33cd2d8cedacdb
程思源 <chengsiyuan@acoinfo.com>
2023-12-22 15:07:36 +0800

    add file transfer test case

---
+++ client/client_file_transfer_test.go
@@ -0,0 +1,153 @@
+package client
+
+import (
+	"bytes"
+	"crypto/md5"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"flag"
+	"io"
+	"testing"
+	"time"
+
+	"gitee.com/sylixos/go-vsoa/protocol"
+	"gitee.com/sylixos/go-vsoa/server"
+)
+
+type FileTransferTestParam struct {
+	FileName string `json:"file_name"`
+	FileSize int    `json:"file_size"`
+}
+
+var (
+	file_transfer_addr = flag.String("file_transfer_addr", "localhost:3006", "file_transfer server address")
+)
+
+//go:embed vsoa.png
+var orginalFile []byte
+
+func TestFileTransfer(t *testing.T) {
+	startFileStreamServer(t)
+	flag.Parse()
+
+	// Do this to make sure the server is ready on slow machine
+	time.Sleep(50 * time.Millisecond)
+
+	var StreamTunID uint16
+
+	clientOption := Option{
+		Password: "123456",
+	}
+
+	c := NewClient(clientOption)
+	_, err := c.Connect("vsoa", *file_transfer_addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	req := protocol.NewMessage()
+	DstParam := new(FileTransferTestParam)
+
+	reply, err := c.Call("/download", protocol.TypeRPC, protocol.RpcMethodGet, req)
+	if err != nil {
+		if err == strErr(protocol.StatusText(protocol.StatusInvalidUrl)) {
+			t.Log("Pass: Invalid URL")
+		} else {
+			t.Fatal(err)
+		}
+	} else {
+		StreamTunID = reply.TunID()
+		t.Log("Seq:", reply.SeqNo(), "Stream TunID:", StreamTunID)
+
+		json.Unmarshal(reply.Param, DstParam)
+		t.Log("FileName:", DstParam.FileName, "FileSize:", DstParam.FileSize)
+	}
+
+	receiveBuf := bytes.NewBufferString("")
+
+	streamDone := make(chan error)
+
+	cs, err := c.NewClientStream(StreamTunID)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		go func() {
+			buf := make([]byte, 32*1024)
+			for {
+				n, err := cs.Read(buf)
+				if err != nil {
+					// EOF means stream cloesed
+					if err == io.EOF {
+						streamDone <- err
+						break
+					} else {
+						streamDone <- err
+						break
+					}
+				}
+				receiveBuf.Write(buf[:n])
+
+				remainingSize := DstParam.FileSize - receiveBuf.Len()
+
+				if remainingSize <= 0 {
+					break
+				}
+			}
+			cs.conn.Close()
+
+			if md5.Sum(orginalFile) != md5.Sum(receiveBuf.Bytes()) {
+				streamDone <- errors.New("Stream file md5 not match!")
+			}
+
+			t.Log("stream download file done successfully!")
+			streamDone <- nil
+		}()
+	}
+
+	d := <-streamDone
+	if d != nil {
+		t.Fatal(d)
+	}
+
+	cs.conn.Close()
+}
+
+func startFileStreamServer(t *testing.T) {
+	// Init golang server
+	serverOption := server.Option{
+		Password: "123456",
+	}
+	s := server.NewServer("golang VSOA stream server", serverOption)
+
+	// Register URL
+	h := func(req, res *protocol.Message) {
+		p := new(FileTransferTestParam)
+		p.FileName = "vsoa_after.png"
+		p.FileSize = len(orginalFile)
+
+		var err error
+
+		res.Param, err = json.Marshal(p)
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		ss, _ := s.NewServerStream(res)
+		pushBuf := bytes.NewBuffer(orginalFile)
+		receiveBuf := bytes.NewBufferString("")
+		go func() {
+			ss.ServeListener(pushBuf, receiveBuf)
+			//t.Log("stream server receiveBuf:", receiveBuf.String())
+		}()
+	}
+	s.On("/download", protocol.RpcMethodGet, h)
+
+	go func() {
+		_ = s.Serve("127.0.0.1:3006")
+	}()
+	//defer s.Close()
+	// Done init golang stream server
+}
